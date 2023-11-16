483ccdf6801ed47b883b2a96b74d93d1151851be
tyohit <150989444+tyohit@users.noreply.github.com>
2023-11-16 09:20:03 +0500

    Add files via upload
    игра говно, но играйте, кто не поиграет - тот гей

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1700107881,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/Черепаха-sheet0.png",
+		"images/Звезда-sheet0.png",
+		"images/Камень-sheet0.png",
+		"images/Обитатель-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
